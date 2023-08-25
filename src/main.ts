2e159ecf8477cb65c56a559589f8afca9a08f902
ang.li <twelve1997@126.com>
2023-08-25 13:55:58 +0800

    init: 自动导入/默认样式/pinia

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index'
-import pinia from '@/store/index'
+import pinia from '@/stores/index'
 import 'normalize.css'
 import '@/assets/style/base.less'
 const app = createApp(App)
