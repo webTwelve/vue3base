ba9add89d89e1cb619c725a8ec5337a119c7ad78
ang.li <twelve1997@126.com>
2023-08-30 15:26:09 +0800

    feat: Icon自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+// 自动导入Icon图标
+import IconsResolver from 'unplugin-icons/resolver'
+import Icons from 'unplugin-icons/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -15,10 +18,24 @@ export default defineConfig({
       dts: 'src/auto-imports.d.ts',
       imports: ['vue', 'vue-router'],
 
-      resolvers: [ElementPlusResolver()]
+      resolvers: [
+        ElementPlusResolver(),
+        IconsResolver({
+          prefix: 'Icon'
+        })
+      ]
     }),
     Components({
-      resolvers: [ElementPlusResolver()]
+      resolvers: [
+        ElementPlusResolver(), // 自动注册图标组件
+        IconsResolver({
+          // 指定collection，即指定为elementplus图标集ep
+          enabledCollections: ['ep']
+        })
+      ]
+    }),
+    Icons({
+      autoInstall: true
     })
   ],
   resolve: {
